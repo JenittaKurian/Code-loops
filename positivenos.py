df275aec2e21b2e3a70181fc9d6569f62a4cc102
JenittaKurian <72756632+JenittaKurian@users.noreply.github.com>
2020-10-13 22:34:09 +0530

    Add files via upload

---
+++ positivenos.py
@@ -0,0 +1,14 @@
+list1 = [12, -7, 5, 64, -14] 
+print("list1=",list1)
+
+for n in list1: 
+      
+    if n >= 0: 
+       print(n, end = " ") 
+list2 = [12, 14, -95, 3] 
+print("\nlist2=",list2)
+
+for n in list2: 
+      
+    if n >= 0: 
+       print(n, end = " ") 
